c23d37336cbc07863513420c2b257ee92eaa0e74
Kulcsár Zsolt <kulcsarzsolt15@gmail.com>
2025-02-25 19:39:16 +0200

    Initialize react

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return <h2>Functional Arrow Component</h2>;
+}
+
+export default App;
